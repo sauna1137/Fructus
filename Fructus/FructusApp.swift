ed8a7289185dcde1624bd03aabe2545c2321aa4b
sauna1137 <subscription.search@gmail.com>
2024-07-11 07:32:20 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by KS on 2024/07/11.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
