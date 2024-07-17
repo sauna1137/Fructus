cd915afcd8458ebc6a343da2e09c98c76f3c9b93
sauna1137 <subscription.search@gmail.com>
2024-07-17 19:44:42 +0900

    additional

--- Fructus/Model/FruitModel.swift
+++ Fructus/Model/FruitModel.swift
@@ -5,4 +5,14 @@
 //  Created by KS on 2024/07/16.
 //
 
-import Foundation
+import SwiftUI
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutritions: [String]
+}
