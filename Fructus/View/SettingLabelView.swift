b9ac5aedab1989a977c822a52b5f4c7aa4f88e46
sauna1137 <subscription.search@gmail.com>
2024-08-13 07:38:24 +0900

    Add SettingView, SettingLabelView

---
+++ Fructus/View/SettingLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingLabelView.swift
+//  Fructus
+//
+//  Created by KS on 2024/08/13.
+//
+
+import SwiftUI
+
+struct SettingLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingLabelView()
+}
